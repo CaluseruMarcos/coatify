179e01cc14d66a07e166601bc4082ff1adac9964
CaluseruMarcos <snorlaxking310@gmail.com>
2026-09-25 17:01:48 +0200

    added files

--- coatify/obj/Debug/net10.0/coatify.AssemblyInfo.cs
+++ coatify/obj/Debug/net10.0/coatify.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coatify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cca2f2b05033943b0633faab7cdccab00f90bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae0e71e0338b1d7a287f19a0d52b6f108603d8f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("coatify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coatify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
